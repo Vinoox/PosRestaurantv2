92134d6ebf7c09f1b7c362a3ffc87d836998e979
Maciej <Maciej@DESKTOP-BIKB829>
2026-06-17 20:31:34 +0200

    conterization

--- src/backend/Services/Identity/Identity.API/Program.cs
+++ src/backend/Services/Identity/Identity.API/Program.cs
@@ -84,7 +84,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseExceptionHandler();
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
